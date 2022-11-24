956c2e92a0df4e4b96acd7699c386e0e934b8657
imdariprayoga27 <114724783+imdariprayoga27@users.noreply.github.com>
2022-11-24 22:35:41 +0800

    Add files via upload

---
+++ session_login/config.php
@@ -0,0 +1,15 @@
+<!--Menghubungkan Database dengan Website-->
+<?php 
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$database = "session_login";
+ 
+$conn = mysqli_connect($server, $user, $pass, $database);
+ 
+if (!$conn) {
+    die("<script>alert('Gagal tersambung dengan database.')</script>");
+}
+ 
+?>
